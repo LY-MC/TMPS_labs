18cbb0f528cf68ad5ba3ab69a2a982c5ec39ba6d
Lesenco Maria <leshenko.mri@gmail.com>
2023-11-07 08:49:08 +0200

    Refactored main

--- src/main/java/com/utm/Main.java
+++ src/main/java/com/utm/Main.java
@@ -2,60 +2,9 @@ package com.utm;
 
 import com.utm.animals.facade.ZooFacade;
 
-import java.util.Scanner;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-
 public class Main {
     public static void main(String[] args) {
         ZooFacade zooFacade = new ZooFacade();
-        Scanner scanner = new Scanner(System.in);
-
-        ExecutorService executor = Executors.newFixedThreadPool(2); // Two threads: one for simulation and one for user input.
-        Future<?> simulationTask = null;
-
-        while (true) {
-            System.out.println("1. Start Simulation");
-            System.out.println("2. Create Checkpoint");
-            System.out.println("3. Restore from Checkpoint");
-            System.out.println("4. Exit");
-            System.out.println("Select an option: ");
-            System.out.println();
-
-            int choice = scanner.nextInt();
-
-            switch (choice) {
-                case 1:
-                    if (simulationTask == null || simulationTask.isDone()) {
-                        simulationTask = executor.submit(() -> {
-                            try {
-                                zooFacade.startSimulation();
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                        });
-                    } else {
-                        System.out.println("Simulation is already running.");
-                    }
-                    break;
-                case 2:
-                    zooFacade.pauseSimulationAndSaveCheckpoint();
-                    System.out.println("Checkpoint created.");
-                    break;
-                case 3:
-                    System.out.print("Enter checkpoint index: ");
-                    int checkpointIndex = scanner.nextInt();
-                    zooFacade.restoreSimulationFromCheckpoint(checkpointIndex);
-                    System.out.println("Restored from checkpoint " + checkpointIndex);
-                    break;
-                case 4:
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("Invalid choice. Try again.");
-                    break;
-            }
-        }
+        zooFacade.runSimulationWithUserInput();
     }
 }
--- src/main/java/com/utm/animals/facade/ZooFacade.java
+++ src/main/java/com/utm/animals/facade/ZooFacade.java
@@ -9,11 +9,16 @@ import com.utm.zooworkers.SecurityGuard;
 import com.utm.zooworkers.Veterinarian;
 import com.utm.zooworkers.Zookeeper;
 
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class ZooFacade {
     private final Simulation simulation;
     private final TimeObserver timeObserver = new TimeObserver();
 
-    public ZooFacade()  {
+    public ZooFacade() {
         CageDimensions cageDimensions = new CageDimensions();
         Cage cageWithLake = Cage.builder(cageDimensions)
                 .withLake()
@@ -28,7 +33,7 @@ public class ZooFacade {
         Cage cage = Cage.builder(cageDimensions)
                 .build();
         simulation = new Simulation(
-                cageWithTrees, cageWithLake, cage , cageWithTreesAndDoubleFencing, new Cashier(),
+                cageWithTrees, cageWithLake, cage, cageWithTreesAndDoubleFencing, new Cashier(),
                 new Veterinarian(), new Zookeeper(), new SecurityGuard()
         );
 
@@ -45,5 +50,54 @@ public class ZooFacade {
 
     public void restoreSimulationFromCheckpoint(int checkpointIndex) {
         simulation.restoreToCheckpoint(checkpointIndex);
+    }
+
+    public void runSimulationWithUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        ExecutorService executor = Executors.newFixedThreadPool(2); // Two threads: one for simulation and one for user input.
+        Future<?> simulationTask = null;
+
+        while (true) {
+            System.out.println("1. Start Simulation");
+            System.out.println("2. Create Checkpoint");
+            System.out.println("3. Restore from Checkpoint");
+            System.out.println("4. Exit");
+            System.out.println("Select an option: ");
+            System.out.println();
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    if (simulationTask == null || simulationTask.isDone()) {
+                        simulationTask = executor.submit(() -> {
+                            try {
+                                startSimulation();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        });
+                    } else {
+                        System.out.println("Simulation is already running.");
+                    }
+                    break;
+                case 2:
+                    pauseSimulationAndSaveCheckpoint();
+                    System.out.println("Checkpoint created.");
+                    break;
+                case 3:
+                    System.out.print("Enter checkpoint index: ");
+                    int checkpointIndex = scanner.nextInt();
+                    restoreSimulationFromCheckpoint(checkpointIndex);
+                    System.out.println("Restored from checkpoint " + checkpointIndex);
+                    break;
+                case 4:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice. Try again.");
+                    break;
+            }
+        }
     }
 }
